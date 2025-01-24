d716948acc7b70e3f1e122c7ca950e40a0212658
Hyperblast <dev@bitflow.org>
2025-01-24 15:59:15 +0500

    add pause --off

--- src/CommandLineTool/Commands/PauseCommand.cs
+++ src/CommandLineTool/Commands/PauseCommand.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Beefweb.Client;
 using Beefweb.CommandLineTool.Services;
 using McMaster.Extensions.CommandLineUtils;
 
@@ -11,6 +12,9 @@ public class PauseCommand(IClientProvider clientProvider) : ServerCommandBase(cl
     [Option("-t|--toggle", Description = "Toggle pause state instead of pausing")]
     public bool Toggle { get; set; }
 
+    [Option("-f|--off", Description = "Disable paused state (does nothing if not paused)")]
+    public bool Off { get; set; }
+
     public override async Task OnExecuteAsync(CancellationToken ct)
     {
         await base.OnExecuteAsync(ct);
@@ -18,10 +22,21 @@ public class PauseCommand(IClientProvider clientProvider) : ServerCommandBase(cl
         if (Toggle)
         {
             await Client.TogglePause(ct);
+            return;
         }
-        else
+
+        if (Off)
         {
-            await Client.Pause(ct);
+            var state = await Client.GetPlayerState(null, ct);
+
+            if (state.PlaybackState == PlaybackState.Paused)
+            {
+                await Client.PlayCurrent(ct);
+            }
+
+            return;
         }
+
+        await Client.Pause(ct);
     }
 }
