c5d04160e636e79a297731f6c872b9bb97ecd6ff
Hyperblast <dev@bitflow.org>
2025-01-27 13:23:12 +0500

    AddCommand: allow reading from stdin

--- src/CommandLineTool/Commands/AddCommand.cs
+++ src/CommandLineTool/Commands/AddCommand.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Beefweb.Client;
@@ -9,7 +10,7 @@ namespace Beefweb.CommandLineTool.Commands;
 
 [Command("add", Description = "Add playlist items",
     UnrecognizedArgumentHandling = UnrecognizedArgumentHandling.CollectAndContinue)]
-public class AddCommand(IClientProvider clientProvider) : ServerCommandBase(clientProvider)
+public class AddCommand(IClientProvider clientProvider, IConsole console) : ServerCommandBase(clientProvider)
 {
     [Option(T.Playlist, CommandOptionType.SingleValue, Description = D.PlaylistToAddTo)]
     public PlaylistRef Playlist { get; set; } = PlaylistRef.Current;
@@ -26,18 +27,44 @@ public class AddCommand(IClientProvider clientProvider) : ServerCommandBase(clie
     [Option("-r|--replace", Description = "Replace existing items in the playlist")]
     public bool Replace { get; set; }
 
+    [Option("-i|--stdin", Description = "Read items from standard input")]
+    public bool ReadFromStdin { get; set; }
+
     public string[]? RemainingArguments { get; set; }
 
     public override async Task OnExecuteAsync(CancellationToken ct)
     {
         await base.OnExecuteAsync(ct);
 
-        if (RemainingArguments is not { Length: > 0 })
+        var items = new List<string>();
+
+        if (RemainingArguments != null)
         {
-            throw new InvalidRequestException("At least one item is required");
+            items.AddRange(items);
         }
 
-        await Client.AddPlaylistItems(Playlist, RemainingArguments, new AddPlaylistItemsOptions
+        if (ReadFromStdin)
+        {
+            var line = await console.In.ReadLineAsync(ct);
+            while (line != null)
+            {
+                line = line.Trim();
+
+                if (line.Length > 0 && line[0] != '#')
+                {
+                    items.Add(line);
+                }
+
+                line = await console.In.ReadLineAsync(ct);
+            }
+        }
+
+        if (items.Count == 0)
+        {
+            throw new InvalidRequestException("At least one item is required.");
+        }
+
+        await Client.AddPlaylistItems(Playlist, items, new AddPlaylistItemsOptions
         {
             TargetPosition = Replace ? null : Position,
             ProcessAsynchronously = NoWait,
