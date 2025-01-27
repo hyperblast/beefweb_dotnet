9bc4bc29d1090686fb09ef1a49d10797843fc5a9
Hyperblast <dev@bitflow.org>
2025-01-27 13:32:26 +0500

    ListCommand: add option to display indices

--- src/CommandLineTool/Commands/ListCommand.cs
+++ src/CommandLineTool/Commands/ListCommand.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ namespace Beefweb.CommandLineTool.Commands;
 public class ListCommand(IClientProvider clientProvider, ISettingsStorage storage, ITabularWriter writer)
     : ServerCommandBase(clientProvider)
 {
+    private static readonly bool[] ItemIndicesColumnAlign = [true];
+
     [Option(T.Playlist, CommandOptionType.SingleValue, Description = D.PlaylistToUse)]
     public PlaylistRef Playlist { get; set; } = PlaylistRef.Current;
 
@@ -25,12 +28,19 @@ public class ListCommand(IClientProvider clientProvider, ISettingsStorage storag
     [Option(T.ItemColumns, Description = D.PlaylistItemColumns)]
     public string[]? ItemColumns { get; set; }
 
+    [Option("-d|--indices", Description = "Display item indices")]
+    public bool ShowIndices { get; set; }
+
     public override async Task OnExecuteAsync(CancellationToken ct)
     {
         await base.OnExecuteAsync(ct);
 
         var columns = ItemColumns.GetOrDefault(storage.Settings.ListFormat);
         var result = await Client.GetPlaylistItems(Playlist, new PlaylistItemRange(ItemIndex, Limit), columns, ct);
-        writer.WriteTable(result.Items.Select(i => i.Columns.ToArray()).ToArray());
+        var rows = ShowIndices
+            ? result.Items.Select((i, k) => (string[]) [k.ToString(CultureInfo.InvariantCulture), ..i.Columns])
+            : result.Items.Select(i => i.Columns.ToArray());
+
+        writer.WriteTable(rows.ToArray(), ShowIndices ? ItemIndicesColumnAlign : null);
     }
 }
